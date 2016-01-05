07996612262b1b847195b1d3e76513a22ec76041
chibitofu <erinmoony@gmail.com>
2016-01-04 17:08:15 -0800

    Created validations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
+
+class LinkValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      record.errors[attribute] << (options[:message] || "is not a link")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :title,
+  presence: true,
+  length: { in: 10...100}
+
+  validates :link,
+  presence: true,
+  link: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
+  validates :email,
+  presence: true,
+  email: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :name,
+  presence: true,
+  length: { maximum: 19 }
 end
